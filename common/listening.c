0d476d1ca217c129c9bdd97214c651efe64fecc5
c64cosmin <c64cosmin@gmail.com>
2016-03-06 23:37:37 +0200

    Added implementation for listening

--- common/listening.c
+++ common/listening.c
@@ -1,59 +1,43 @@
-#include "../common/connection.h"
+#include "common/listening.h"
+#include <sys/types.h>
+#include <sys/socket.h>
 #include <stdio.h>
 
-SOCKET_HANDLE connect_to_ip(char* address, int port){
-}
-
-void send_data(SOCKET_HANDLE sh, void* data, int data_size){
-}
-
-void* recv_data(SOCKET_HANDLE sh, int* data_size){
-}
-
-SOCKET_HANDLE create_socket(char* address, int port){
-    SOCKET_HANDLE sh;
-    sh.socketfd = socket(AF_INET, SOCK_STREAM, 0);
+void listen(char* address, int port){
+    int socketfd;
+    
+    socketfd = socket(AF_INET, SOCK_STREAM, 0);
     
     //if socketfd is -1 we have a error
-    if(sh.socketfd == -1){
+    if(socketfd == -1){
         printf("ERROR: Cannot create the socket.\n");
-        sh.socketfd = -1;
-        return sh;
+        return;
     }
 
-    struct addrinfo addr;
+    struct sockaddr_in addr;
     addr.sin_family = AF_INET;
     addr.sin_port = htons(port);
     addr.sin_addr.s_addr = inet_addr(adress);
-    memset(my_addr.sin_zero, 0, sizeof(my_addr.sin_zero);
+    memset(addr.sin_zero, 0, sizeof(addr.sin_zero);
 
     //bind the socket to the requested address
-    int errorno = bind(sh.socketfd, &addr, sizeof(addr));
+    int errorno = bind(socketfd, &addr, sizeof(addr));
 
     if(errorno == -1){
         printf("ERROR: Cannot bind the socket for %s:%i\n", address, port);
-        sh.socketfd = -1;
-        return sh;
-    }
-    
-    return sh;
-}
-
-SOCKET_HANDLE listen_for_connection(SOCKET_HANDLE sh){
-    SOCKET_HANDLE new_sh;
-    int errorno;
-    
-    errorno = listen(sh.socketfd, 10);
-
-    if(errorno == -1){
-        printf("ERROR: Cannot listen on the socket\n");
-        new_sh.socketfd = -1;
-        return new_sh;
+        return;
     }
 
-    struct addrinfo addr;
-    int addr_size = sizeof(addr);
-    new_sh.socketfd = accept(sh, &addr, &addr_size);
+    socklen_t addr_size;
+    sockaddr_storage new_addr;
+    int newsocketfd;
 
-    return new_sh;
+    while(true){
+        listen(socketfd, 10);
+        addr_size = sizeof(new_addr);
+        newsocketfd = accept(socketfd, (struct sockaddr*)&new_addr, &addr_size);
+
+        char *new_ip = inet_ntoa(((sockaddr_in)new_addr).sin_addr);
+        printf("%s has connected\n", new_ip);
+    }
 }
---
+++ common/listening.h
@@ -0,0 +1,5 @@
+#ifndef _LISTENING_H_
+#define _LISTENING_H_
+
+void listen(char* address, int port);
+#endif //_LISTENING_H_
